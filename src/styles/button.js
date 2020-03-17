7e7c0766d8b46d71f1c39b5c0bd3daa3ab8383ea
nikhil arora <narora200@gmail.com>
2020-03-17 22:42:08 +0530

    styles: change button feedback

--- src/styles/button.js
+++ src/styles/button.js
@@ -30,15 +30,20 @@ export const buttonDisabled = css`
 `;
 
 export const buttonFeedback = css`
-  transition: all 0.3s;
+  transition: all 0.15s ease-in-out;
+  text-align: center;
   :hover {
-    transform: scale(1.03);
+    transform: translateY(-1px);
+    box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
   }
   :active {
-    transform: scale(0.98);
+    transform: translateY(1px);
+    box-shadow: 0 6px 12px -2px rgba(50, 50, 93, 0.25),
+      0 3px 7px -3px rgba(0, 0, 0, 0.3);
   }
   :disabled:hover,
   :disabled:active {
     transform: none;
+    box-shadow: none;
   }
 `;
